2138fde862b3be62098da50d69917648b6710516
Jeremy Mikkola <jeremy@jeremymikkola.com>
2015-08-23 10:54:28 -0700

    Fix building jar

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :main ^:skip-aot markov.core
+  :aot [markov.core]
+  :main markov.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
